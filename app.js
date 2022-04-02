7fbb2107af54185edc92d24585b877cd159115cb
Vanessa Bragdon <vanessabragdon@Vanessas-MBP.home>
2022-04-02 11:54:49 -0700

    add all exercise files

---
+++ app.js
@@ -0,0 +1,22 @@
+// var PI = 3.14;
+// PI = 42; stop me from doing this!
+const PI = 3.14
+PI = 42
+
+
+// What is the difference between var and let?
+// var can be redeclared and let cannot.
+// var follows the rules of function scope while let is block scope
+
+
+// What is the difference between var and const?
+// var can be reassigned and redeclared and const cannot.
+// var follows the rules of function scope while const is block scope
+
+
+// What is the difference between let and const?
+// let can be reassigned and const cannot.
+
+
+// What is hoisting?
+// JS will run all the 'var' declared variables first before anything else. Using var, you can access the variable name and its undefined value and define it later on.
